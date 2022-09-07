9bad537dff07665d7d3b93ff1e29982bd3a8aa95
Kent <90602074+Qnode20@users.noreply.github.com>
2022-09-08 00:42:37 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import moment from 'moment';
 
 export default function App() {
   const [advice, setAdvice] = useState('');
+  const formatDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 
   const fetchAdvice = () => {
     axios
@@ -24,6 +26,7 @@ export default function App() {
 
   return (
     <div className="app">
+      <h3 className="heading">{formatDate}</h3>
       <div className="card">
         <h3 className="heading" index={advice.id}>
           {advice.advice}
